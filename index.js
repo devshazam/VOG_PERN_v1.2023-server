616440e38e9adde74c7b3cf331b7221656691d58
Jack Lee <113794114+jacklee137@users.noreply.github.com>
2023-06-12 13:24:45 +0300

    1

--- index.js
+++ index.js
@@ -10,39 +10,25 @@ const path = require('path')
 
 
 
-const https = require("https");
-// const fs = request("fs");
-
-
-
-
-
-
-
-
-  const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
 const app = express()
 
-const fs = require("fs");
 
 // ###################### middleware #############################
 // Подключение посредников, порядок подключения влияет на исполнение кода
 app.use(cors({
-  origin: ['https://kopi34.ru/', 'https://api.kopi34.ru/']
+  origin: 'https://kopi34.ru'
 }));
 
-app.use((req, res, next) => {
-  res.set('Access-Control-Allow-Origin', 'https://kopi34.ru');
-  // res.set('Access-Control-Allow-Origin', 'https://kopi34.ru');
-  // res.set('Access-Control-Allow-Credentials', 'true');
-  // res.set('Content-Type', 'text/plain')
-  next();
-});
+// app.use((req, res, next) => {
+//   res.set('Access-Control-Allow-Origin', 'https://kopi34.ru');
+//   // res.set('Access-Control-Allow-Origin', 'https://kopi34.ru');
+//   // res.set('Access-Control-Allow-Credentials', 'true');
+//   // res.set('Content-Type', 'text/plain')
+//   next();
+// });
 
-// @header( 'Content-Type: text/html; charset=' . get_option( 'blog_charset' ) );
-// @header( 'Access-Control-Allow-Origin: https://domain.com' ); // Без слеша в конце. Не знаю почему, но это важно
-// @header( 'Access-Control-Allow-Credentials: true' );
 
 
 app.use(express.json())
@@ -66,17 +52,7 @@ app.get('/', (req, res) => {
 
 
 
-const options = {
-  key: fs.readFileSync("server.key"),
-  cert: fs.readFileSync("server.cert"),
-};
-  
 
 
-
-// https.createServer(options, app)
-// .listen(5000, function (req, res) {
-//   console.log("Server started at port 5000");
-// });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
