3e79daf0dc5b9088dcad8a2fedc82a778c2bf4e5
Jack Lee <113794114+jacklee137@users.noreply.github.com>
2023-06-30 08:50:40 +0300

    build: работа над аутентификацией

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,25 +1,18 @@
-
 const jwt = require('jsonwebtoken')
 
 module.exports = function (req, res, next) {
-    // return res.status(401).json(req.headers)
     if (req.method === "OPTIONS") {
         next()
     }
-
     try {
         const token = req.headers.authorization.split(' ')[1] // Bearer asfasnfkajsfnjk
-        // return res.status(401).json({token})
         if (!token) {
-            return res.status(401).json({message: "Не авторизован1"})
+            return res.status(401).json({message: "Не авторизован"})
         }
-        
         const decoded = jwt.verify(token, process.env.SECRET_KEY)
-
         req.user = decoded
         next()
     } catch (e) {
-        res.json({fjf: 23})
-        // res.status(401).json({message: "Не авторизован1"})
+        res.status(401).json({message: "Не авторизован"})
     }
 };
