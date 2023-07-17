ac7c55d692c906005974492a7570c58d81417707
Jack Lee <113794114+jacklee137@users.noreply.github.com>
2023-07-17 20:23:00 +0300

    2323

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -18,156 +18,207 @@ const fetch = require("node-fetch");
  */
 
 class DeviceController {
-  // (1)  POST - http://localhost:5000/api/device/ - Покупка отдельных товаров с занесением в базу данных
-  async testFirst(req, res, next) {
-    // Done
-    const { value, description, tel } = req.body;
-    // console.log(req.files.img)
-    // const img = req.files.img;
-    // const fileName = uuid.v4() + ".jpg"
-    // img.mv(path.resolve(__dirname, '..', 'static', fileName))
-    return res.json({ value, description, tel });
-  }
+    // (1)  POST - http://localhost:5000/api/device/ - Покупка отдельных товаров с занесением в базу данных
+    async testFirst(req, res, next) {
+        // Done
+        const { value, description, tel } = req.body;
+        // console.log(req.files.img)
+        // const img = req.files.img;
+        // const fileName = uuid.v4() + ".jpg"
+        // img.mv(path.resolve(__dirname, '..', 'static', fileName))
+        return res.json({ value, description, tel });
+    }
 
-  // (2)  POST - http://localhost:5000/api/device/ - Покупка отдельных товаров с занесением в базу данных
-  async homePage(req, res, next) {
-    // Done
+    // (2)  POST - http://localhost:5000/api/device/ - Покупка отдельных товаров с занесением в базу данных
+    async homePage(req, res, next) {
+        // Done
 
-    const { name, value, description, userId } = req.body;
+        const { name, value, description, userId } = req.body;
 
-    const img = req.files.img;
-    const fileName = uuid.v4() + ".jpg";
-    img.mv(path.resolve(__dirname, "..", "static", fileName));
+        const img = req.files.img;
+        const fileName = uuid.v4() + ".jpg";
+        img.mv(path.resolve(__dirname, "..", "static", fileName));
 
-    const user = await User.findOne({ where: { id: userId } });
-    const userDescription = `Имя клиента: ${user.name}; ID-клиента: ${user.id}; Телефон клиента: ${user.phone}; Email клиента: ${user.email};`;
+        const user = await User.findOne({ where: { id: userId } });
+        const userDescription = `Имя клиента: ${user.name}; ID-клиента: ${user.id}; Телефон клиента: ${user.phone}; Email клиента: ${user.email};`;
 
-    const device = await Device.create({
-      name,
-      feature: description,
-      userDescription: userDescription,
-      img: fileName,
-      userId,
-    });
-
-    // Send to YOOMONEY
-    const payV = String(value);
-    const IdempotenceKey = uuid.v4();
-    const headersP = {
-      "Content-Type": "application/json",
-      "Idempotence-Key": IdempotenceKey,
-      // TODO - Заменить открытые пароли на env
-      Authorization:
-        "Basic " +
-        btoa(process.env.MARKET_ID + ":" + process.env.SECRET_KEY_UMONEY),
-    };
-    const inputBodyP = {
-      amount: {
-        value: payV,
-        currency: "RUB",
-      },
-      capture: true,
-      // "payment_method_data": {
-      //     "type": 'bank_card'
-      // },
-      confirmation: {
-        type: "redirect",
-        return_url: "https://kopi34.ru/payinfo/",
-      },
-      description: device.id,
-      metadata: {
-        order_id: device.id,
-      },
-    };
-
-    fetch("https://api.yookassa.ru/v3/payments", {
-      method: "POST",
-      body: JSON.stringify(inputBodyP),
-      headers: headersP,
-    })
-      .then(function (res) {
-        return res.json();
-      })
-      .then(function (body) {
-        return res.json(body);
-      }).catch((e) => {
-        return next(ApiError.badRequest(`Ошибка вызова Юманни на сервере (deviceController.homePage): ${e.code} + ${e.message}`))
-      });
-  }
-
-  // (3) POST: - http://localhost:5000/api/device/getpay - подтверждение оплаты заказа
-  async getPay(req, res, next) {
-    // Done
-
-    const headersP = {
-      Authorization:
-        "Basic " +
-        btoa(process.env.MARKET_ID + ":" + process.env.SECRET_KEY_UMONEY),
-    };
-
-    const { payinfo, orderid } = req.body;
-
-    await User.update({ status_pay: true }, { where: { id: orderid } });
-
-    console.log(payinfo);
-    fetch("https://api.yookassa.ru/v3/payments/" + payinfo, {
-      method: "GET",
-      headers: headersP,
-    })
-      .then(function (res) {
-        return res.json();
-      })
-      .then(function (body) {
-        return res.json(body);
-      }).catch((e) => {
-        return next(ApiError.badRequest(`Ошибка вызова Юманни на сервере (deviceController.homePage): ${e.code} + ${e.message}`))
-      });
-  }
-
-  // (4) GET: - http://localhost:5000/api/device/admin/devices-view/ - Просмотр всех заказов
-  async allOrdersAdmin(req, res, next) {
-    // Done
-    // createdAt
-
-    let { itemSort, orderSort, limit, page, id, filter } = req.query;
-    page = page || 1;
-    limit = limit || 10;
-    itemSort = itemSort || "ASC";
-    orderSort = orderSort || "createdAt";
-    let offset = page * limit - limit;
-    let devices;
-    console.log(id);
-    if (id)
-      if (id == "0") {
-        devices = await Device.findAndCountAll({
-          where: { status_pay: true, name: filter },
-          order: [[itemSort, orderSort]],
-          limit,
-          offset,
+        const device = await Device.create({
+            name,
+            feature: description,
+            userDescription: userDescription,
+            img: fileName,
+            userId,
         });
-      } else {
-        devices = await Device.findAndCountAll({
-          where: { id: id },
-          order: [[itemSort, orderSort]],
-          limit,
-          offset,
-        });
+
+        // Send to YOOMONEY
+        const payV = String(value);
+        const IdempotenceKey = uuid.v4();
+        const headersP = {
+            "Content-Type": "application/json",
+            "Idempotence-Key": IdempotenceKey,
+            // TODO - Заменить открытые пароли на env
+            Authorization:
+                "Basic " +
+                btoa(
+                    process.env.MARKET_ID + ":" + process.env.SECRET_KEY_UMONEY
+                ),
+        };
+        const inputBodyP = {
+            amount: {
+                value: payV,
+                currency: "RUB",
+            },
+            capture: true,
+            // "payment_method_data": {
+            //     "type": 'bank_card'
+            // },
+            confirmation: {
+                type: "redirect",
+                return_url: "https://kopi34.ru/payinfo/",
+            },
+            description: device.id,
+            metadata: {
+                order_id: device.id,
+            },
+        };
+
+        fetch("https://api.yookassa.ru/v3/payments", {
+            method: "POST",
+            body: JSON.stringify(inputBodyP),
+            headers: headersP,
+        })
+            .then(function (res) {
+                return res.json();
+            })
+            .then(function (body) {
+                return res.json(body);
+            })
+            .catch((e) => {
+                return next(
+                    ApiError.badRequest(
+                        `Ошибка вызова Юманни на сервере (deviceController.homePage): ${e.code} + ${e.message}`
+                    )
+                );
+            });
+    }
+
+    // (3) POST: - http://localhost:5000/api/device/getpay - подтверждение оплаты заказа
+    async getPay(req, res, next) {
+        // Done
+
+        const headersP = {
+            Authorization:
+                "Basic " +
+                btoa(
+                    process.env.MARKET_ID + ":" + process.env.SECRET_KEY_UMONEY
+                ),
+        };
+
+        const { payinfo, orderid } = req.body;
+
+        await User.update({ status_pay: true }, { where: { id: orderid } });
+
+        console.log(payinfo);
+        fetch("https://api.yookassa.ru/v3/payments/" + payinfo, {
+            method: "GET",
+            headers: headersP,
+        })
+            .then(function (res) {
+                return res.json();
+            })
+            .then(function (body) {
+                return res.json(body);
+            })
+            .catch((e) => {
+                return next(
+                    ApiError.badRequest(
+                        `Ошибка вызова Юманни на сервере (deviceController.homePage): ${e.code} + ${e.message}`
+                    )
+                );
+            });
+    }
+
+    // (4) GET: - http://localhost:5000/api/device/admin/devices-view/ - Просмотр всех заказов
+
+    async allOrdersAdmin(req, res, next) {
+        let { itemSort, orderSort, limit, page, id, filter, userId } = req.query;
+        console.log(userId)
+        page = page || 1;
+        limit = limit || 10;
+        itemSort = itemSort || "ASC";
+        orderSort = orderSort || "createdAt";
+        let offset = page * limit - limit;
+        let devices;
+        // console.log(id);
+        // if (id)
+            if(userId){
+                  try{devices = await Device.findAndCountAll({
+                    where: { userId: userId },
+                    order: [[itemSort, orderSort]],
+                    limit,
+                    offset});
+                  return res.json(devices);
+                }catch(e){
+                      return next(
+                        ApiError.badRequest(
+                            `Ошибка БД1 (deviceController.allOrdersAdmin): ${e.code} + ${e.message}`
+                        )
+                    );
+                }
+            }
+            if (id == "0") {
+                try{devices = await Device.findAndCountAll({
+                    where: { status_pay: true, name: filter },
+                    order: [[itemSort, orderSort]],
+                    limit,
+                    offset});
+                    return res.json(devices);
+                  }catch(e){
+                    return next(
+                      ApiError.badRequest(
+                          `Ошибка БД2 (deviceController.allOrdersAdmin): ${e.code} + ${e.message}`
+                      )
+                  );
+                  }
+            } else {
+              try{devices = await Device.findAndCountAll({
+                  where: { id: id },
+                  order: [[itemSort, orderSort]],
+                  limit,
+                  offset});
+                  return res.json(devices);
+              }catch(e){
+                return next(
+                  ApiError.badRequest(
+                    `Ошибка БД3 (deviceController.allOrdersAdmin): ${e.code} + ${e.message}`
+                  )
+              );
+              }
+            }
+        
+    }
+
+    // (4) POST: - http://localhost:5000/api/device/delete-item/ - Просмотр всех заказов
+    async deleteOrdersAdmin(req, res, next) {
+        // Done
+        // createdAt
+        const { id } = req.body;
+        try{
+          const device = await Device.update(
+            { status_done: true },
+            { where: { id } }
+        );
+
+        return res.json(device);
+      }catch(e){
+        return next(
+          ApiError.badRequest(
+            `Ошибка БД (deviceController.deleteOrdersAdmin): ${e.code} + ${e.message}`
+          )
+        );
       }
-    return res.json(devices);
-  }
-
-  // (4) POST: - http://localhost:5000/api/device/delete-item/ - Просмотр всех заказов
-  async deleteOrdersAdmin(req, res, next) {
-    // Done
-    // createdAt
-    const { id } = req.body;
-    const device = await Device.update(
-      { status_done: true },
-      { where: { id } }
-    );
-
-    return res.json(device);
-  }
+        
+    }
 }
 
 module.exports = new DeviceController();
