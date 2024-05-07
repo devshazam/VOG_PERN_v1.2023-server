7d8629768dabe5fffb412302643f336540abbadd
shazam-dev <9956221@gmail.com>
2024-05-07 11:54:10 +0300

    fix 2

--- index.js
+++ index.js
@@ -11,45 +11,25 @@ const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 const path = require('path')
 
-
-
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
-
-
-
-
-// ###################### middleware #############################
-// Подключение посредников, порядок подключения влияет на исполнение кода
-// app.use(cors({
-//   origin: ["https://kopi34.ru","http://localhost:3000"] 
-// }));
 app.use(cors());
 
 app.use(express.json())
 
+// app.use(express.static(path.resolve(__dirname, 'static')))
+app.use('/api', express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
 
-
-// Подключение хранилища файлов и системы их загрузки
-app.use(express.static(path.resolve(__dirname, 'static')))
-app.use(fileUpload({}))
-
-
-
-// Подключать предпоследним! - роутер
 app.use('/api', router) 
-// Подключать последним! - обработка ошибок.
+
 app.use(errorHandler)
 
 app.get('/api', (req, res) => {
   res.send("GET Request Called")
 })
-
-// ###################### END ##################################
-
-
 
 const start = async () => {
   try {
